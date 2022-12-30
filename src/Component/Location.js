9712d8b4dfaff29919d4ed79f06b5153d1e5b770
AntaresScorpius <rohitc171@gamil.com>
2022-12-30 20:26:32 +0530

    Clean Up and Build

--- src/Component/Location.js
+++ src/Component/Location.js
@@ -1,17 +1,6 @@
 import React from "react";
 
 function Location({data, ip}) {
-  // const temp = {
-  //   ip: "103.141.87.219",
-  //   city: "Jalgaon",
-  //   region: "Maharashtra",
-  //   country: "IN",
-  //   loc: "20.7899,75.7558",
-  //   org: "AS133311 Maxtech Broadband Pvt Ltd",
-  //   postal: "425203",
-  //   timezone: "Asia/Kolkata",
-  //   readme: "https://ipinfo.io/missingauth",
-  // };
   console.log("data saved");
   console.log(data);
 
