dc53dc0e8dfdd49f4e1a8f5c73d12616c9990ddb
AntaresScorpius <rohitc171@gamil.com>
2022-12-30 20:17:16 +0530

    Final Build

--- src/Component/Map.js
+++ src/Component/Map.js
@@ -3,8 +3,8 @@ import React from "react";
 const Map = ({loc = ""}) => {
   let url = `https://maps.google.com/maps?q=${loc}&z=15&output=embed`;
   return (
-    <div style={{marginTop: 10}}>
-      <iframe src={url} width="700" height="270" title="Maps"></iframe>
+    <div style={{marginTop: 20}}>
+      <iframe src={url} width="700" height="300" title="Maps"></iframe>
     </div>
   );
 };
--- src/Component/Pincode.js
+++ src/Component/Pincode.js
@@ -5,17 +5,17 @@ function Pincode({pins, text}) {
   return (
     <div className="pin-flex-container">
       {/* <input className="input" type="text"></input> */}
-      {pins[0]?.PostOffice?.filter((list) => list.Name.includes(text)).map(
-        (item) => (
-          <article key={item.Name} className="pin-item">
-            <p>Name: {item.Name}</p>
-            <p>Branch Type: {item.BranchType} </p>
-            <p>Dilevery Status: {item.DeliveryStatus}</p>
-            <p>District: {item.District}</p>
-            <p>Division: {item.Division}</p>
-          </article>
-        )
-      )}
+      {pins[0]?.PostOffice?.filter((list) =>
+        list.Name.toLowerCase().includes(text.toLowerCase())
+      ).map((item) => (
+        <article key={item.Name} className="pin-item">
+          <p>Name: {item.Name}</p>
+          <p>Branch Type: {item.BranchType} </p>
+          <p>Dilevery Status: {item.DeliveryStatus}</p>
+          <p>District: {item.District}</p>
+          <p>Division: {item.Division}</p>
+        </article>
+      ))}
     </div>
   );
 }
