b0a8094784def0fec9683a13bfd117522ecddc19
AntaresScorpius <rohitc171@gamil.com>
2022-12-30 20:49:59 +0530

    Final

--- src/Component/Location.js
+++ src/Component/Location.js
@@ -6,10 +6,14 @@ function Location({data, ip}) {
 
   return (
     <div className="location-flex">
-      <article className="location-item">Lat : {data?.loc}</article>
+      <article className="location-item">
+        Lat : {data?.loc.split(",")[0]}
+      </article>
       <article className="location-item">City : {data?.city}</article>
       <article className="location-item">Organisation : {data?.org}</article>
-      <article className="location-item">Long : {data?.loc}</article>
+      <article className="location-item">
+        Long : {data?.loc.split(",")[1]}
+      </article>
       <article className="location-item">Region : {data?.region}</article>
       <article className="location-item">Hostname : {data?.ip}</article>
     </div>
--- src/Component/Map.js
+++ src/Component/Map.js
@@ -4,7 +4,7 @@ const Map = ({loc = ""}) => {
   let url = `https://maps.google.com/maps?q=${loc}&z=15&output=embed`;
   return (
     <div style={{marginTop: 20}}>
-      <iframe src={url} width="700" height="300" title="Maps"></iframe>
+      <iframe src={url} width="300" height="300" title="Maps"></iframe>
     </div>
   );
 };
